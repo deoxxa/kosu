3b5b6d62206dd2594a0dca95cc21efad2f213744
deoxxa <deoxxa@fknsrs.biz>
2013-07-30 18:00:37 +1000

    make the example a bit nicer to read

--- example.js
+++ example.js
@@ -8,8 +8,7 @@ var query = {
     y: true,
   },
   condition: {type: "and", data: [
-    {type: "eq", left: "x", right: 0},
-    {type: "eq", left: "y", right: 0},
+    {type: "eq", left: "x", right: 3},
     {type: "and", data: [
       {type: "gt", left: "info.time", right: "2012-08-24T00:00:00.000Z"},
       {type: "lt", left: "info.time", right: "2012-08-24T23:59:59.999Z"},
@@ -19,12 +18,17 @@ var query = {
 
 var kosu = new Kosu(query);
 
+console.warn("compiled filter function:");
+console.warn("");
 console.warn(kosu.filter.toString());
+console.warn("");
+
+console.warn("matching data:");
 console.warn("");
 
 kosu.on("data", function(e) {
   console.log(e);
 });
 
-kosu.write({x: 0, y: 0});
-kosu.write({x: 0, y: 0, info: {time: "2012-08-24T10:00:00.000Z"}});
+kosu.write({x: 3, y: 6});
+kosu.write({x: 3, y: 7, info: {time: "2012-08-24T10:00:00.000Z"}});
